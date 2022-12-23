be0684e36689f91f6d59120f4b04eddd25f0a33c
slkc0724 <88265542+slkc0724@users.noreply.github.com>
2022-12-23 20:48:11 +0800

    Add files via upload

---
+++ christmas_tree_name.py
@@ -0,0 +1,90 @@
+import turtle
+turtle.shape("turtle")
+turtle.speed(5)
+
+def star(turtle, color, size, x, y):
+	turtle.penup()
+	turtle.goto(x,y)
+	turtle.pendown()
+	turtle.color(color)
+	turtle.fillcolor(color)
+	turtle.begin_fill()
+	for i in range(5):
+		turtle.right(75)
+		turtle.forward(size * 2)
+		turtle.left(75)
+		turtle.forward(size * 2)
+		turtle.right(72)
+	turtle.end_fill()
+
+def tree_first(turtle, color):
+	turtle.penup()
+	turtle.left(89.5)
+	turtle.backward(56)
+	turtle.left(180)
+	turtle.pendown()
+	turtle.color(color)
+	turtle.fillcolor(color)
+
+def tree_second(turtle):
+	size = 50
+	for i in range(3):
+		turtle.begin_fill()
+		turtle.right(30)
+		for i in range(3):
+			turtle.forward(size * 3)
+			turtle.left(120)
+		turtle.end_fill()
+		turtle.left(30)
+		turtle.penup()
+		turtle.forward(88)
+		turtle.pendown()
+		size = (size / 2) * 3
+
+def tree_base(turtle, color, size):
+	turtle.forward(205)
+	turtle.pendown()
+	turtle.color(color)
+	turtle.fillcolor(color)
+	turtle.pendown()
+	turtle.begin_fill()
+	turtle.left(90)
+	turtle.forward(size / 2)
+	turtle.right(90)
+	turtle.forward((size / 2) * 3)
+	turtle.right(90)
+	turtle.forward(size)
+	turtle.right(90)
+	turtle.forward((size / 2) * 3)
+	turtle.right(90)
+	turtle.forward(size / 2)
+	turtle.end_fill()
+	turtle.penup()
+	turtle.color("red")
+	turtle.left(180)
+	turtle.forward(size)
+	turtle.forward(75)
+	turtle.right(90)
+	turtle.forward(400)
+	turtle.right(90)
+	turtle.write("Merry", move = True, align = "center", font = ('Bradley Hand', 48, "normal"))
+	turtle.forward(275)
+	turtle.write("Christmas", move = True, align = "center", font = ('Bradley Hand', 48, "normal"))
+
+def name():
+	turtle.color("pink")
+	turtle.right(90)
+	turtle.forward(500)
+	turtle.right(90)
+	turtle.forward(40)
+	turtle.right(180)
+	turtle.write("By Cathy SO", move = True, align = "center", font = ('Bradley Hand', 25, "normal"))
+	turtle.hideturtle()
+
+star(turtle, "yellow", 15, 0, 300)
+tree_first(turtle, "green")
+tree_second(turtle)
+tree_base(turtle, "brown", 70)
+name()
+
+turtle.done()
